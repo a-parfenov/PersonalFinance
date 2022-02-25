6aa19e818b51732ce571d9ec23b04b14b4da78f3
Parfenov <v5pishka@yandex.ru>
2022-02-25 16:13:36 +0300

    Создал HashMap для разных языков (пока только для русского)

---
+++ src/PersonalFinance/PersonalFinance.java
@@ -0,0 +1,27 @@
+package PersonalFinance;
+
+import settings.Text;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class PersonalFinance {
+    public static void main(String[] args) {
+        init();
+        System.out.println(Text.get("PROGRAM_NAME"));
+        System.out.println(Arrays.toString(Text.getMonths()));
+    }
+    private static void init() {
+        try {
+            Text.init();
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("fonts/Roboto-Light.ttf")));
+        } catch (FontFormatException | IOException e) {
+            Logger.getLogger(PersonalFinance.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+}
