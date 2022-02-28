e4460e372d869b54b4fa1e4c7e3073d97100ac46
Parfenov <v5pishka@yandex.ru>
2022-02-28 15:16:29 +0300

    Создал классы для хранения и перевода средств

--- src/personalfinance/model/Currency.java
+++ src/personalfinance/model/Currency.java
@@ -2,6 +2,8 @@ package personalfinance.model;
 
 import personalfinance.exception.ModelException;
 
+import java.util.Objects;
+
 public class Currency extends Common {
     private String title;
     private String code;
@@ -11,12 +13,93 @@ public class Currency extends Common {
 
     public Currency() {}
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public void setOn(boolean on) {
+        isOn = on;
+    }
+
+    public void setBase(boolean base) {
+        isBase = base;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public boolean isOn() {
+        return isOn;
+    }
+
+    public boolean isBase() {
+        return isBase;
+    }
+
+    @Override
+    public String toString() {
+        return "Currency{" +
+                "title='" + title + '\'' +
+                ", code='" + code + '\'' +
+                ", rate=" + rate +
+                ", isOn=" + isOn +
+                ", isBase=" + isBase +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Currency currency = (Currency) o;
+
+        return code.equals(currency.code);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 25 * hash + Objects.hashCode(this.code);
+        return hash;
+    }
+
     public Currency(String title, String code, double rate, boolean isOn, boolean isBase) throws ModelException {
         if (title.length()  == 0) throw new ModelException(ModelException.TITLE_EMPTY);
+        if (code.length()  == 0) throw new ModelException(ModelException.CODE_EMPTY);
+        if (rate  <= 0) throw new ModelException(ModelException.RATE_INCORRECT);
+
         this.title = title;
         this.code = code;
         this.rate = rate;
         this.isBase = isBase;
         this.isOn = isOn;
+    }
+
+    @Override
+    public String getValueForComboBox() {
+        return title;
+    }
+
+    public double getRateByCurrency(Currency currency) {
+        return rate / currency.rate;
     }
 }
---
+++ src/personalfinance/model/Transactions.java
@@ -0,0 +1,107 @@
+package personalfinance.model;
+
+import personalfinance.exception.ModelException;
+
+import java.util.Date;
+
+public class Transactions extends Common {
+
+    private Account account;
+    private Article article;
+    private double amount;
+    private String notice;
+    private Date date;
+
+    public Transactions() {}
+
+    public Transactions(Account account, Article article, double amount, String notice, Date date) throws ModelException {
+        if (account == null) throw new ModelException(ModelException.ACCOUNT_EMPTY);
+        if (article == null) throw new ModelException(ModelException.ARTICLE_EMPTY);
+        this.account = account;
+        this.article = article;
+        this.amount = amount;
+        this.notice = notice;
+        this.date = date;
+    }
+
+    public Transactions(Account account, Article article, double amount) throws ModelException {
+        if (account == null) throw new ModelException(ModelException.ACCOUNT_EMPTY);
+        if (article == null) throw new ModelException(ModelException.ARTICLE_EMPTY);
+        this.account = account;
+        this.article = article;
+        this.amount = amount;
+        this.notice = "";
+        this.date = new Date();
+    }
+
+    public Transactions(Account account, Article article, double amount, String notice) throws ModelException {
+        if (account == null) throw new ModelException(ModelException.ACCOUNT_EMPTY);
+        if (article == null) throw new ModelException(ModelException.ARTICLE_EMPTY);
+        this.account = account;
+        this.article = article;
+        this.amount = amount;
+        this.notice = notice;
+        this.date = new Date();
+    }
+
+    public Transactions(Account account, Article article, double amount, Date date) throws ModelException {
+        if (account == null) throw new ModelException(ModelException.ACCOUNT_EMPTY);
+        if (article == null) throw new ModelException(ModelException.ARTICLE_EMPTY);
+        this.account = account;
+        this.article = article;
+        this.amount = amount;
+        this.notice = "";
+        this.date = date;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public Article getArticle() {
+        return article;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getNotice() {
+        return notice;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public void setArticle(Article article) {
+        this.article = article;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public void setNotice(String notice) {
+        this.notice = notice;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Transactions{" +
+                "account=" + account +
+                ", article=" + article +
+                ", amount=" + amount +
+                ", notice='" + notice + '\'' +
+                ", date=" + date +
+                '}';
+    }
+}
